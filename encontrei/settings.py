42b421f34f232276d1969f571922b3b4d0f74839
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2019-06-17 15:34:09 -0300

    URL Fix

--- encontrei/settings.py
+++ encontrei/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['https://api-encontrei.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['api-encontrei.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
