fec6b97528dcc2b10f6cc52e5d6074bc67a0edfa
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2019-06-17 16:54:23 -0300

    Estabelecimento Model Created

--- estabelecimento/models.py
+++ estabelecimento/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from categoria.models import Categoria
 
-# Create your models here.
+
+class Estabelecimento(models.Model):
+    class Meta:
+        db_table = 'tb_estabelecimento'
+
+    id = models.AutoField(primary_key=True, editable=False, auto_created=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, editable=False)
+    titulo = models.CharField(max_length=50)
+    imagem = models.URLField(max_length=255)
+    endereco = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=50)
+    cep = models.IntegerField(max_length=8)
+    cidade = models.CharField(max_length=50)
+    uf = models.CharField(max_length=2)
+    telefone = models.IntegerField(max_length=11)
+    email = models.EmailField()
+    sobre = models.TextField(max_length=255)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    whatsapp = models.IntegerField(max_length=11)
+    facebook = models.URLField(max_length=100)
+
+    database = 'db_encontrei'
+
+    def __str__(self):
+        return self.titulo
